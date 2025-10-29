4831f1828d96741d24034c1b7dcb5eeb7fb90c8f
donFranko99 <franciszek.rybski@microsoft.wsei.edu.pl>
2025-10-29 16:52:39 +0100

    added cleaning for replica directory and some additional messages

--- FolderSync/MessagesIO.cs
+++ FolderSync/MessagesIO.cs
@@ -29,6 +29,24 @@ public static class MessagesIO
         return 10;
     }
 
+    public static bool AskForReplicaClean(bool isClean)
+    {
+        if (!isClean)
+        {
+            System.Console.WriteLine("Your replica directory is not empty.\nTo proceed with the sync it needs to be clean.\nDo you want to clean it now? (type y to clean)");
+            string? shouldClean = Console.ReadLine();
+            switch (shouldClean)
+            {
+                case "y":
+                    PrintCleaningMessage();
+                    return true;
+                default:
+                    return false;
+            }
+        }
+        return false;
+    }
+
     public static void PrintStartingProcessMessage()
     {
         System.Console.WriteLine("Starting synchronization process...");
@@ -37,6 +55,16 @@ public static class MessagesIO
     public static void SynchronisationCompleteMessage()
     {
         System.Console.WriteLine("Synchronization complete");
+    }
+
+    public static void PrintAbortMessage()
+    {
+        System.Console.WriteLine("Operation aborted");
+    }
+
+    public static void PrintCleaningMessage()
+    {
+        System.Console.WriteLine("Cleaning replica directory");
     }
     
     public static void PrintEscapeMessage()
--- FolderSync/SyncOperator.cs
+++ FolderSync/SyncOperator.cs
@@ -2,24 +2,41 @@ public static class SyncOperator
 {
     public static void SyncFolders(string sourcePath, string replicaPath)
     {
-        //TODO: clear replica first
-
         var subDirectories = Directory.GetDirectories(sourcePath);
         var sourceFiles = Directory.EnumerateFiles(sourcePath);
+
+        //TODO: clear replica first
 
         foreach (var file in sourceFiles)
         {
             File.Copy(file, replicaPath);
         }
-        
-        if(subDirectories.Count != 0)
+
+        if (subDirectories.Count() != 0)
         {
-            foreach(var dir in subDirectories)
+            foreach (var dir in subDirectories)
             {
-                string replicaSubdirectoryName = replicaPath + dir.Split("\\").Last;
+                string replicaSubdirectoryName = replicaPath + dir.Split("\\").Last();
                 Directory.CreateDirectory(replicaSubdirectoryName);
                 SyncFolders(dir, replicaSubdirectoryName);
             }
         }
+    }
+    
+    static void CleanReplica(string replicaPath)
+    {
+        var replicaSubDirectories = Directory.GetDirectories(replicaPath);
+        var replicaFiles = Directory.EnumerateFiles(replicaPath);
+
+        foreach (var file in replicaFiles)
+        {
+            File.Delete(file);
+        }
+        foreach (var dir in replicaSubDirectories)
+        {
+            CleanReplica(dir);
+            Directory.Delete(dir);
+        }
+        
     }
 }
